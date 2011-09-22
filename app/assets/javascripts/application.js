051c0f274a135f0a570d31e3ec160fb7d6226f9a
Ken Nakagawa <nakagawk@gmail.com>
2011-09-22 05:11:18 -0700

    JSLint cleanup and softtabbing css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,11 @@
 var numIsEven = function(n) {
-  return (n % 2 == 0) ? true : false;
+  return (n % 2 === 0) ? true : false;
 };
 
 $(document).ready(function() {
   $('article').each(function() {
-    if (numIsEven($(this).attr('id')))
-      $(this).addClass('zebra')
-  })
-})
+    if (numIsEven(this.attr('id'))) {
+      $(this).addClass('zebra');
+    }
+  });
+});
